871baecad6dce931d542eccebd91b8ea4f1255fc
David Montesdeoca <backpackerhh@gmail.com>
2014-08-16 21:36:20 +0100

    Improve search functionality.

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,9 @@
 module ArticlesHelper
+  def highlighted_title(article)
+    if article.try(:highlight).try(:title)
+      article.highlight.title[0].html_safe
+    else
+      article.title
+    end
+  end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -3,6 +3,35 @@ require 'elasticsearch/model'
 class Article < ActiveRecord::Base
   include Elasticsearch::Model
   include Elasticsearch::Model::Callbacks
+
+  def self.search(query)
+    __elasticsearch__.search(
+      {
+        query: {
+          multi_match: {
+            query: query,
+            fields: ['title^10', 'text']
+          }
+        },
+        highlight: {
+          pre_tags: ['<em>'],
+          post_tags: ['</em>'],
+          fields: {
+            title: {},
+            text: {}
+          }
+        }
+      }
+    )
+  end
+
+  settings index: { number_of_shards: 1 } do
+    mappings dynamic: 'false' do
+      indexes :title, analyzer: 'english', index_options: 'offsets'
+      indexes :text,  analyzer: 'english'
+    end
+  end
 end
 
-Article.import # for auto sync model with elastic search
+# Index all article records from the DB to Elasticsearch
+Article.import force: true
